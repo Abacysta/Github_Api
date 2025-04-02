37483a41f75c7895cd2f3a6f4e39a87314169d45
Abacysta <140728690+Abacysta@users.noreply.github.com>
2025-04-02 05:21:47 +0200

    setup
    about:
    Java 21
    Spring boot 3.4.4
    Gradle - Groovy
    
    Dependencies:
    -lombok
    -spring web

---
+++ src/main/java/com/example/Github_Api/GithubApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Github_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Github_Api/GithubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Github_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
